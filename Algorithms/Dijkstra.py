b023bb7093cdea0c8651389bd16effd66891cb89
shreyadate05 <shreyadate05@gmail.com>
2020-11-06 14:50:52 -0500

    working - checked with a couple of examples

--- Algorithms/Dijkstra.py
+++ Algorithms/Dijkstra.py
@@ -3,6 +3,17 @@ costs = {}
 parents = {}
 processed = []
 
+def print_shortest_path(start, fin):
+    path = []
+    path.append(fin)
+    n = fin
+    while parents[n] != start and parents[n] != None:
+        path.append(parents[n])
+        n = parents[n]
+    path.append(start)
+    path = path[::-1]
+    return "-".join(path)
+
 def find_lowest_cost_nodes():
     lowest_cost = float("inf")
     lowest_cost_node = None
@@ -44,5 +55,114 @@ def setup():
     parents["B"] = "start"
     parents["Fin"] = None
 
+def setup2():
+    graph["start"] = {}
+    graph["start"]["A"] = 5
+    graph["start"]["B"] = 2
+    graph["A"] = {}
+    graph["A"]["C"] = 4
+    graph["A"]["D"] = 2
+    graph["B"] = {}
+    graph["B"]["A"] = 8
+    graph["B"]["D"] = 7
+    graph["C"] = {}
+    graph["C"]["D"] = 6
+    graph["C"]["Fin"] = 3
+    graph["D"] = {}
+    graph["D"]["Fin"] = 1
+    graph["Fin"] = {}
+
+    costs["start"] = 0
+    costs["A"] = 5
+    costs["B"] = 2
+    costs["C"] = float("inf")
+    costs["D"] = float("inf")
+    costs["Fin"] = float("inf")
+
+    parents["A"] = "start"
+    parents["B"] = "start"
+    parents["C"] = None
+    parents["D"] = None
+    parents["Fin"] = None
+
+def setup3():
+    graph["start"] = {}
+    graph["start"]["A"] = 10
+    graph["A"] = {}
+    graph["A"]["C"] = 20
+    graph["B"] = {}
+    graph["B"]["A"] = 1
+    graph["C"] = {}
+    graph["C"]["B"] = 1
+    graph["C"]["Fin"] = 30
+    graph["Fin"] = {}
+
+    costs["start"] = 0
+    costs["A"] = 10
+    costs["B"] = float("inf")
+    costs["C"] = float("inf")
+    costs["Fin"] = float("inf")
+
+    parents["A"] = "start"
+    parents["B"] = None
+    parents["C"] = None
+    parents["Fin"] = None
+
+def setup4():
+    graph["start"] = {}
+    graph["start"]["A"] = 2
+    graph["start"]["B"] = 2
+    graph["A"] = {}
+    graph["A"]["C"] = 2
+    graph["A"]["Fin"] = 2
+    graph["B"] = {}
+    graph["B"]["A"] = 2
+    graph["C"] = {}
+    graph["C"]["B"] = -1
+    graph["C"]["Fin"] = 2
+    graph["Fin"] = {}
+
+    costs["start"] = 0
+    costs["A"] = 2
+    costs["B"] = 2
+    costs["C"] = float("inf")
+    costs["Fin"] = float("inf")
+
+    parents["A"] = "start"
+    parents["B"] = "start"
+    parents["C"] = None
+    parents["Fin"] = None
+
 if __name__ == '__main__':
     setup()
+    dijkstras()
+    print("Weights of the shortest path is: ", costs["Fin"])
+    print("Shortest path is: ", print_shortest_path("start", "Fin"))
+
+    processed = []
+    graph = {}
+    costs = {}
+    parents = {}
+    setup2()
+    dijkstras()
+    print("Weights of the shortest path is: ", costs["Fin"])
+    print("Shortest path is: ", print_shortest_path("start", "Fin"))
+
+    processed = []
+    graph = {}
+    costs = {}
+    parents = {}
+    setup3()
+    dijkstras()
+    print("Weights of the shortest path is: ", costs["Fin"])
+    print("Shortest path is: ", print_shortest_path("start", "Fin"))
+
+    processed = []
+    graph = {}
+    costs = {}
+    parents = {}
+    setup4()
+    dijkstras()
+    print("Weights of the shortest path is: ", costs["Fin"])
+    print("Shortest path is: ", print_shortest_path("start", "Fin"))
+
