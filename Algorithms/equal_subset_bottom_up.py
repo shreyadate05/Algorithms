e02da8e581e61f6429a3192b4871399614c4fa6d
shreyadate05 <shreyadate05@gmail.com>
2020-11-11 15:12:49 -0500

    top-down approach with memoization

---
+++ Algorithms/equal_subset_bottom_up.py
@@ -0,0 +1,28 @@
+
+def canPartition(arr):
+    if sum(arr) % 2 != 0:
+        return False
+
+    s = int(sum(arr)/2)
+    dp = [[False for j in range(s+1)] for i in range(len(arr))]
+
+    for i in range(s+1):
+        dp[0][i] = (arr[0] == i)
+
+    for i in range(len(arr)):
+        dp[i][0] = True
+
+    for i in range(len(arr)):
+        for j in range(s+1):
+            if arr[i] <= j:
+                dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i]]
+            else:
+                dp[i][j] = dp[i-1][j]
+
+    print(dp)
+    return dp[len(arr)-1][s]
+
+if __name__ == "__main__":
+    ans = canPartition([1,2,3,4])
+    print(ans)
+    # [[-1, -1, -1, -1, -1, 1], [-1, -1, -1, -1, 1, -1], [-1, -1, 0, -1, 1, -1], [-1, 0, 0, -1, 1, -1]]
